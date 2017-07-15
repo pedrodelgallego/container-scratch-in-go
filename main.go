65802308e6f6e75b6109d1bb2ec05d56f3ff88c3
Pedro Gallego <pedro.delgallego@gmail.com>
2017-07-16 01:31:05 +0200

    run the command in a child process

--- main.go
+++ main.go
@@ -10,16 +10,17 @@ import (
 func main() {
 	switch os.Args[1] {
 	case "run":
-			run()
-		
+		run()
+	case "child":
+		child()
 	default:
-			panic("help")
+		panic("help")
 	}
 }
 
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2])
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -27,6 +28,18 @@ func run() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
+	
+	must(cmd.Run())
+}
+
+
+func child() {
+	fmt.Printf("Running %v\n", os.Args[2])
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	must(syscall.Sethostname([]byte("container")))
 	
 	must(cmd.Run())
 }

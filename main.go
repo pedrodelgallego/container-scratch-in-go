ab6daa67dc94cb8457fc3f3eec51bdfd26916306
Pedro Gallego <pedro.delgallego@gmail.com>
2017-07-16 01:37:50 +0200

    refactor code

--- main.go
+++ main.go
@@ -1,51 +1,60 @@
 package main
 
 import (
-	"os/exec"
-	"os"
-	"fmt"
-	"syscall"
+    "fmt"
+    "os"
+    "os/exec"
+    "syscall"
 )
 
 func main() {
-	switch os.Args[1] {
-	case "run":
-		run()
-	case "child":
-		child()
-	default:
-		panic("help")
-	}
+    fmt.Printf("Running %v\n", os.Args[2])
+
+    switch os.Args[1] {
+    case "run":
+        run()
+    case "child":
+        child()
+    default:
+        panic("help")
+    }
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2])
-	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-	}
-	
-	must(cmd.Run())
+    cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+    cmd = setStdInputsAndOutputs(cmd)
+    cmd = setNameSpaces(cmd)
+
+    must(cmd.Run())
 }
 
-
 func child() {
-	fmt.Printf("Running %v\n", os.Args[2])
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	must(syscall.Sethostname([]byte("container")))
-	
-	must(cmd.Run())
+    cmd := exec.Command(os.Args[2], os.Args[3:]...)
+    cmd = setStdInputsAndOutputs(cmd)
+
+    must(syscall.Sethostname([]byte("container")))
+    must(syscall.Chroot("/home/liz/ubuntufs"))
+    must(os.Chdir("/"))
+
+    must(cmd.Run())
+}
+
+func setNameSpaces(cmd *exec.Cmd) *exec.Cmd {
+    cmd.SysProcAttr = &syscall.SysProcAttr{
+        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+    }
+    return cmd
+}
+
+func setStdInputsAndOutputs(cmd *exec.Cmd) *exec.Cmd {
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    return cmd
 }
 
 func must(err error) {
-	if err != nil {
-		panic(err)
-	}
+    if err != nil {
+        panic(err)
+    }
 }
